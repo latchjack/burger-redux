5265abe2598e352f2cd48b35b7578b63f11a6b16
Latch Jackn <latch.jack@gmail.com>
2021-03-25 08:28:39 +0000

    Added basics file

--- redux-basics.js
+++ redux-basics.js
@@ -7,9 +7,31 @@ const initialState = {
 
 // Reducer
 const rootReducer = (state = initialState, action) => {
+    if (action.type === 'INC_COUNTER') {
+        return {
+            ...state,
+            counter: counter + 1
+        }
+    };
+    if (action.type === 'ADD_COUNTER') {
+        return {
+            ...state,
+            counter: counter + action.value
+        }
+    }
     return state;
 };
 
 // Store
 const store = createStore(rootReducer);
+console.log(store.getState());
+
+// Subscription
+store.subscribe(() => {
+    console.log('[subscription]', store.getState());
+});
+
+// Dispatching Action
+store.dispatch({ type: 'INC_COUNTER' });
+store.dispatch({ type: 'ADD_COUNTER', value: 10 });
 console.log(store.getState());
